a623d67ca9b9764b5830eaeb76d9250afb93c932
Harsh Thakur <harshthakur0025@gmail.com>
2024-07-27 15:15:05 +0000

    added signup button  to the app bar

--- apps/web/components/appbar.tsx
+++ apps/web/components/appbar.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export function Appbar() {
+    const router = useRouter();
   return (
     <div className="flex text-white bg-black p-5 space-x-5">
       <button onClick={()=>signIn()}>SignIn</button>
       <button onClick={()=>signOut()}>SignOut</button>
+      <button onClick={()=>router.push("/signup")}>SignUp</button>
     </div>
   );
 }
