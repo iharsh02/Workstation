b6018fa057ea966ec114a21faa69a124931ac762
Harsh Thakur <harshthakur0025@gmail.com>
2024-07-28 16:07:24 +0000

    added routes to upload and get the file from AWS s3

--- apps/backend/src/router/workspace/workstation.ts
+++ apps/backend/src/router/workspace/workstation.ts
@@ -1,12 +1,55 @@
 import express from "express";
+import {
+  GetObjectCommand,
+  S3Client,
+  ListObjectsV2Command,
+} from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+dotenv.config();
+
+const ACCESSKEY_ID = process.env.ACCESSKEY_ID;
+const SECRETACCESSKEY = process.env.SECRETACCESSKEY;
+const REGION = process.env.REGION;
+const BUCKET = process.env.BUCKET;
+
+if (!ACCESSKEY_ID || !SECRETACCESSKEY || !REGION) {
+  throw new Error("AWS credentials are not defined in environment variables.");
+}
+
+const s3client = new S3Client({
+  region: REGION,
+  credentials: {
+    accessKeyId: ACCESSKEY_ID,
+    secretAccessKey: SECRETACCESSKEY,
+  },
+});
+
+async function getObjectURL(key: any) {
+  const command = new GetObjectCommand({
+    Bucket: BUCKET,
+    Key: key,
+  });
+  const url = await getSignedUrl(s3client, command);
+  return url;
+}
+
+// async function init() {
+//   console.log(`URL for video `, await getObjectURL("uploads/uploads-1722176212652"));
+// }
+// init();
+
 
 export const workstation = express.Router();
-
-workstation.get("/" , (req , res)=>{
-    res.json({
-        message : "hello from workstation route"
+// db.client.workstation.findUnique({})
+workstation.get("/", async (req, res) => {
+    const command = new ListObjectsV2Command({
+      Bucket : BUCKET,
+      Prefix: 'uploads',
     })
-})
-
-
-
+    const result = await s3client.send(command);
+    res.json({
+      result
+    })
+});
--- apps/backend/src/router/workspace/workstationUpload.ts
+++ apps/backend/src/router/workspace/workstationUpload.ts
@@ -1,6 +1,57 @@
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import express, { Request, Response } from "express";
-export const workstationUploads = express.Router();
 
-workstationUploads.post("/",(req : Request , res : Response)=>{
-    // POSt route to the workstation uploads
-})
+const ACCESSKEY_ID = process.env.ACCESSKEY_ID;
+const SECRETACCESSKEY = process.env.SECRETACCESSKEY;
+const REGION = process.env.REGION;
+const BUCKET = process.env.BUCKET;
+
+if (!ACCESSKEY_ID || !SECRETACCESSKEY) {
+  throw new Error("AWS credentials are not defined in environment variables.");
+}
+
+const s3client = new S3Client({
+  region: REGION,
+  credentials: {
+    accessKeyId: ACCESSKEY_ID,
+    secretAccessKey: SECRETACCESSKEY,
+  },
+});
+
+async function putObject(filename: any, contentType: any) {
+  const command = new PutObjectCommand({
+    Bucket: BUCKET,
+    Key: `uploads/${filename}`,
+    ContentType: contentType,
+  });
+
+  const url = await getSignedUrl(s3client, command);
+  return url;
+}
+
+async function init() {
+  console.log(
+    "URL for uploading ",
+    await putObject(`uploads-${Date.now()}`, "upload/jpeg")
+  );
+}
+
+//  to delete a file in s3 bucket 
+/*
+async function deleteFile() {
+    const command = new DeleteObjectCommand({
+        Bucket : BUCKET,
+        Key : 'your_key file location could be upload/jpeg'
+    })
+
+    await s3client.send(command);
+    
+}*/
+
+// init();
+
+export const workstationUploads = express.Router();
+workstationUploads.post("/", (req: Request, res: Response) => {
+  // POSt route to the workstation uploads
+});
